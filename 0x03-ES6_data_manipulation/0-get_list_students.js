638d3e96be72bd25813a50bd29ed82f5865b5e61
israel-09 <israelinene91@gmail.com>
2024-03-11 14:52:53 +0100

    task 0 dependencies update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,18 @@
-
 export default function getListStudents() {
   const guillaume = {
-    'id': 1,
-    'firstName': 'Guillaume',
-    'location': 'San Francisco',
-  }
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
   const james = {
-    'id': 2,
-    'firstName': 'James',
-    'location': 'Columbia',
-  }
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
   const serena = {
-    'id': 3,
-    'firstName': 'Serena',
-    'location': 'San Francisco',
-  }
-  return [guillaume, james, serena]
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  return [guillaume, james, serena];
 }
