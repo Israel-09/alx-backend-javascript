b3d2afb5f393181404c6debd79bbe42c9749a86d
israel-09 <israelinene91@gmail.com>
2024-03-12 15:26:12 +0100

    task 8 solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,11 @@ export default function cleanSet(set, startString) {
   const array = Array.from(set);
   const filtStr = array.filter((elem) => elem.indexOf(startString) === 0);
   if (filtStr.length > 0 && startString.length > 0) {
-    return filtStr.reduce((acc, current) => `${acc}-${getString(current, startString)}`, '');
+    let cleanStr = getString(filtStr[0], startString);
+    for (const elem of filtStr.slice(1)) {
+      cleanStr = `${cleanStr}-${getString(elem, startString)}`;
+    }
+    return cleanStr;
   }
   return '';
 }
