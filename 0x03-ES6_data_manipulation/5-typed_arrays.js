458863bb0f1ae808cfee16197ffd03366cb84905
israel-09 <israelinene91@gmail.com>
2024-03-13 07:01:29 +0100

    handled out of range indexing

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
+  if (position >= length || position < 0) {
+    throw new Error("Position outside range");
+  }
   const dataView = new DataView(buffer);
   dataView.setInt8(position, value);
   return dataView;
