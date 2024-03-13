25e398a615bc38fd782fbfaf0d6057dc2213f017
israel-09 <israelinene91@gmail.com>
2024-03-13 07:03:08 +0100

    fix string quote

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   if (position >= length || position < 0) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   const dataView = new DataView(buffer);
   dataView.setInt8(position, value);
